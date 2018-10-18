6edc7131bd49c49c2fd776f0712c099735eba829
Ingrid Rosselis <irscunha@dcc.ufmg.br>
2018-10-18 15:35:41 -0300

    initial commit

--- router.py
+++ router.py
@@ -2,79 +2,130 @@
 
 import argparse
 import socket
-import json
 import sys
 
 
-def main():
-    # testa número mínimo de argumentos
-    if len(sys.argv) < 4:
-        sys.exit('Incorrect number of parameters!')
-
-    # recebe parâmetros por linha de comando
-    parser = argparse.ArgumentParser('Parâmetros do roteador')
-    parser.add_argument('--addr', type=str, required=True,
-                        help='IP do roteador')
-    parser.add_argument('--update-period', type=float, required=True,
-                        help='Período entre envio de mensagens de update')
-    parser.add_argument('--startup-commands', type=str,
-                        required=False, help='Arquivo de comandos')
-
-    args = parser.parse_args()
-    print(args)
-
-
-# classe que trata todos aspectos do roteador
 class Router:
-    # socket
-    sock = None
-    # endereço de IP do roteador
-    host = None
-    # peso das arestas
-    weight = None
-    # tabela de rotas conhecidas
-    routes = None
     # porta padrão
     port = 55151
+    # endereço de IP do roteador
+    host = ''
+    # tamanho do temporizador entre mensagens de update
+    tout = 0
+    # socket
+    sock = None
+    # tabela de rotas conhecidas
+    linkTable = {'dest': [], 'gateway': [], 'weight': []}
 
     # inicializa roteador
-    def __init__(self, h, w):
-        pass
+    def __init__(self, args):
+        self.host = args.addr
+        self.tout = args.timeout
 
-    # envia mensagens via socket
-    def send(self):
-        pass
+        try:
+            # cria socket UDP
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 
-    # recebe mensagens via socket
-    def get(self):
-        pass
+            # associa o socket ao endereço de IP
+            self.sock.bind((self.host, self.port))
+        except socket.error as error_msg:
+            self.logExit(error_msg)
 
-    # monta mensagem em formato JSON
-    def message(self, d, t, pl=None, dist=None, hp=None):
-        msg = {'source': self.host,
-               'destination': d,
-               'type': t
-               }
+        if args.config:
+            self.setInitialConfig(args.config)
 
-        if t is 'data':
+    # extrai topologia do arquivo
+    def setInitialConfig(self, filename):
+        with open(filename, 'r') as fp:
+            lines = fp.read().splitlines()
+
+            for ln in lines:
+                self.parseCommand(ln)
+
+    # inicia execução do roteador
+    def start(self):
+        while True:
+            self.commandLine()
+            print(self.linkTable)
+
+    # extrai informações de comandos do arquivo e stdin
+    def parseCommand(self, command):
+        cmd = command.split(' ')
+        cmd = [x.strip() for x in cmd if len(x) > 0]
+
+        if cmd[0] == 'add':
+            if cmd[1] not in self.linkTable['dest']:
+                if len(cmd) == 3:
+                    self.addLink(cmd[1], cmd[2])
+                else:
+                    self.addLink(cmd[1])
+            else:
+                print('IP já existe na tabela')
+        elif cmd[0] == 'del':
+            if cmd[1] in self.linkTable['dest']:
+                self.rmvLink(cmd[1])
+            else:
+                print('IP não existe na tabela')
+        elif cmd[0] == 'trace':
+            print()
+        elif cmd[0] == 'quit':
+            sys.exit()
+        elif cmd[0] == 'debug':
+            print()
+        else:
+            print('Comando inválido')
+
+    # recebe input via stdin
+    def commandLine(self):
+        string = input('~ ')
+        self.parseCommand(string)
+
+    # adiciona um link na tabela
+    def addLink(self, d, w=1, g=None):
+        self.linkTable['dest'].append(d)
+        self.linkTable['gateway'].append(g)
+        self.linkTable['weight'].append(w)
+
+    # remove um link da tabela
+    def rmvLink(self, d):
+        index = self.linkTable['dest'].index(d)
+        self.linkTable['dest'].pop(index)
+        self.linkTable['gateway'].pop(index)
+        self.linkTable['weight'].pop(index)
+
+    def buildMessage(self, d, t, pl=None, dist=None, hp=None):
+        msg = {
+            'type': t,
+            'source': self.host,
+            'destination': d
+        }
+
+        if t == 'data':
             msg.update({'payload': pl})
-        elif t is 'update':
+        elif t == 'update':
             msg.update({'distances': dist})
-        elif t is 'trace':
+        elif t == 'trace':
             hp.append(self.host)
-            msg.update({'hops': hp})
+            msg.update({'hops': })
 
         return msg
 
-    # calcula rotas mais curtas
-    def calcRoutes(self):
-        pass
 
-    # tratamento de erros
-    def logExit(self, msg):
-        self.sock.close()
-        sys.exit(msg)
+def main():
+    # recebe parâmetros por linha de comando
+    parser = argparse.ArgumentParser('Parâmetros do roteador')
+    parser.add_argument('--addr', dest='addr', type=str,
+                        required=True, help='IP do roteador')
+    parser.add_argument('--update-period', dest='timeout', type=float,
+                        required=True, help='Período entre envio de mensagens')
+    parser.add_argument('--startup-commands', dest='config',
+                        type=str, required=False, help='Arquivo de comandos')
+    args = parser.parse_args()
+
+    # configura e inicia roteador
+    router = Router(args)
+    router.start()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
